f0c4fd77bea91177f58eed89e11735e17bb20747
luisplis <luisplis@gmail.com>
2026-03-17 20:39:36 +0100

    Lab 3E Frontend Search Filters Page

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -14,6 +14,12 @@ export default function Navbar() {
                     <li className="nav-item">
                         <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
                     </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app/search">Search</a> {/* Updated Link */}
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app/login">Login</a> {/* Updated Link */}
+                    </li>
                 </ul>
             </div>
         </nav>
