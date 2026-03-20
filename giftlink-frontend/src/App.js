1bead0bef21771a19f701c242d21b04e10d54257
luisplis <luisplis@gmail.com>
2026-03-20 14:04:49 +0100

    profile bugs solved

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import LoginPage from './components/LoginPage/LoginPage';
+import Profile from './components/Profile/Profile';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
@@ -21,6 +22,7 @@ function App() {
           <Route path="/app" element={<MainPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/login" element={<LoginPage />} />
+          <Route path="/app/profile" element={<Profile />} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage />} />
         </Routes>
